e2fa5bff0e178d903312fc40a893f464a40c8b38
ManideepArruri <manideeparruri@gmail.com>
2026-02-17 16:00:27 +0530

    Comments Added again

---
+++ Message.java
@@ -0,0 +1,10 @@
+// This App displays a simple message on the console as OOPS
+// @author Developer
+// @version 1
+
+
+class Message{
+	public static void main(String[] args){
+		System.out.println("OOPS");
+	}
+}
