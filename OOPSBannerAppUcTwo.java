ab427685bf88d7821d9e5730ba40a9bcd8a38061
ARRURI MANIDEEP <118426958+ManideepArruri@users.noreply.github.com>
2026-02-18 12:13:26 +0530

    Update and rename MessageConsole.java to OOPSBannerAppUcTwo.java

--- OOPSBannerAppUcTwo.java
+++ OOPSBannerAppUcTwo.java
@@ -2,7 +2,7 @@
 // @author Developer
 // @version 2
 
-class MessageConsole{
+class OOPSBannerAppUcTwo{
 	public static void main(String[] args){
 		System.out.println("  ***   "    +"  ***  "   +"***  "  +"  ****");
 		System.out.println(" *   *  "    +" *   *  "  +"*  *  " +" *");
