6ed61ad302a5527a1c0a2d814ebabd1207dce37c
ARRURI MANIDEEP <118426958+ManideepArruri@users.noreply.github.com>
2026-02-18 12:10:33 +0530

    Update and rename UcThree.java to OOPSBannerAppUcThree.java

--- OOPSBannerAppUcThree.java
+++ OOPSBannerAppUcThree.java
@@ -2,7 +2,7 @@
 // @author Developer
 // @version 3
 
-class UcThree{
+class OOPSBannerAppUcThree{
 	public static void main(String[] args){
 		System.out.println(String.join(" ","  ***   "    ,"  ***    "  ,"***  " ,"  ****"));
 		System.out.println(String.join(" "," *   *  "    ," *   *   "  ,"*  * " ," *"));
